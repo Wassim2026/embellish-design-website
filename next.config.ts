13f1788fe3b645b3f8eafc7ec9ba2a294eb329eb
Wassim2026 <wassimabbasbalouch@gmail.com>
2026-09-23 12:06:36 +0400

    Add production lockfile and image configuration

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,13 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/photo-**",
+      },
+    ],
+  },
+};
 export default nextConfig;
